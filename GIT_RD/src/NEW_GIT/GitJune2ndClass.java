988b01a8bccd5327a03f04250a5baf0e86ec8db6
rd346 <rajatrd346@gmail.com>
2023-06-18 11:28:56 +0530

    added 2 class files of 18th june 2023

---
+++ GIT_RD/src/NEW_GIT/GitJune2ndClass.java
@@ -0,0 +1,11 @@
+package NEW_GIT;
+
+public class GitJune2ndClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("2nd class of git june");
+System.out.println("2nd sop of 2nd class");
+	}
+
+}
---
+++ GIT_RD/src/NEW_GIT/SAMPLE.java
@@ -0,0 +1,7 @@
+package NEW_GIT;
+
+public class SAMPLE {
+public static void main(String[] args) {
+	System.out.println("rajat git file june18");
+}
+}
